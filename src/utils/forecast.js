afdb083710d8df548b1c991bcafb52333372d738
athee <tuncaymaliev2@gmail.com>
2023-01-30 20:52:27 +0300

    init commit

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,17 +1,19 @@
-const request = require('request')
+const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+    const url = `http://api.weatherstack.com/current?access_key=d7023cb32063c5811aca13b25d890278&query=${longitude},${latitude}&units=M`
 
-    request({ url, json: true }, (error, { body }) => {
+    request({ url, json: true },(error, {body}) => {
         if (error) {
-            callback('Unable to connect to weather service!', undefined)
+            callback('Unable to connect to weather services', '')
         } else if (body.error) {
-            callback('Unable to find location', undefined)
-        } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback('Unable to find location', '' )
+        }
+        else{
+            callback('', `${body.current.weather_descriptions[0]}. it is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. The humidity is ${body.current.humidity}.`)
         }
     })
 }
+
 
 module.exports = forecast
